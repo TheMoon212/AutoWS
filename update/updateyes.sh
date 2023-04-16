b9119c426f257844d48680e2c195bcd0858f7b5f
The Moon <122973636+TheMoon212@users.noreply.github.com>
2023-04-17 06:45:31 +0700

    Update updateyes.sh

--- update/updateyes.sh
+++ update/updateyes.sh
@@ -1,1 +1,13 @@
-echo "ga ada update bang"
+#!/bin/bash
+echo -e Proses Update
+#Hapus File Lama
+rm -f /root/versi
+rm -f /usr/bin/domaingratis
+#Download Update
+wget -q -O /usr/bin/domaingratis "https://raw.githubusercontent.com/TheMoon212/AutoWS/main/domaingratis.sh"
+wget -q -O /root/versi "https://raw.githubusercontent.com/TheMoon212/AutoWS/main/update/versinew"
+
+#Permission
+chmod +x /usr/bin/domaingratis
+echo -e "\e[32mUpdate Selesai...\e[0m"
+cd
